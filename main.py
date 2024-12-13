c445078d6934add4ab762a18117621eb5fd98fa5
EvgeniyMsk <logvinov.evgeniy@ya.ru>
2024-12-13 19:30:22 +0300

    init

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 import buzzoola_service as buzz_svc
 import videohead_service as video_svc
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={
+            "buzzoola_income": buzz_svc.get_income(),
+            "buzzoola_stats": buzz_svc.get_stats(),
+            "videohead_stats": video_svc.get_stat()
+        }
+    )
 
 
 @app.get("/buzzoola/income")
@@ -17,5 +32,7 @@ async def buzzoola_stats():
 
 
 @app.get("/videohead/stats")
-async def buzzoola_stats():
+async def videohead_stats():
     return video_svc.get_stat()
+
+
